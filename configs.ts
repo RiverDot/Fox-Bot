1dcb09109e559d88ba3371ecbd72dfd5b01f6e48
RiverDot <72624705+RiverDot@users.noreply.github.com>
2023-06-11 18:28:34 -0400

    Forgot something

--- configs.ts
+++ configs.ts
@@ -3,7 +3,7 @@ import { dotEnvConfig } from './deps.ts'
 // Get the .env file that the user should have created, and get the token
 const env = dotEnvConfig({ export: true, path: './.env' })
 const token = env.BOT_TOKEN || Deno.env.get("BOT_TOKEN")!; 
-const devGuild = BigInt(env.DEV_GUILD_ID!) || Deno.env.get("DEV_GUILD_ID")!;
+const devGuild = BigInt(env.DEV_GUILD_ID || Deno.env.get("DEV_GUILD_ID")!); 
 
 export interface Config {
     token: string
